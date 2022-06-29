415f7778fbd6d2989c53039d0cd72e189bd1cf64
Danijel Lakic <danijellakic@yahoo.com>
2022-06-29 11:31:51 +0200

    new

---
+++ main.js
@@ -0,0 +1,26 @@
+//Navigation toogle
+
+var toggle = document.querySelector('.toggle');
+var navigation = document.querySelector('.navigation');
+
+toggle.addEventListener('click', function() {
+    toggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+});
+
+
+/*  NAVIGATION STICKY AND CHANGES COLOR 
+
+var navbar = document.querySelector('.navigation')
+
+window.onscroll = function() {
+
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 0) {
+        navigation.classList.add('scrolled')
+    } else {
+        navigation.classList.remove('scrolled')
+    }
+}
+
+*/
